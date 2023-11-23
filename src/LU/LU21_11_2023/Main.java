e0b7e060d4d9e3259000aeb9978bd9db53dea000
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-11-23 20:26:45 +0200

    21.11.2023

--- src/LU/LU21_11_2023/Main.java
+++ src/LU/LU21_11_2023/Main.java
@@ -7,8 +7,8 @@ import LU.LU21_11_2023.models.Monster;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        Character character = new Character("Djamaikata", 100, 5, 1);
-        Monster monster = new Monster("Dgeto", 120, 4);
+        Character character = new Character("Djamaikata", 100, 20, 1);
+        Monster monster = new Monster("Dgeto", 800, 4);
         game.addCharacter(character);
         game.addMonster(monster);
         int maxAttacks = 0;
